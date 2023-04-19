72a4bf9f29d8ef71f07c73a6621f9e3a1c8004ab
ShonaliRomeshika <shonaliromeshika@gmail.com>
2023-04-19 11:56:06 +0530

    server.js created

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const cors = require("cors");
+
+
+const app=express();
+
+
+const PORT = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const URL = "mongodb+srv://root:f*5pgJBpqJee5C3@cluster0.dxsdx.mongodb.net/?retryWrites=true&w=majority";
+
+
+mongoose.connect(URL, {
+    useNewUrlParser: "true",
+});
+
+//create a connection
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("Mongodb Connection Success!");
+
+});
+
+app.listen (PORT, () => {
+    console.log(`Server is up and running on port: ${PORT}`)
+})
+
+
+//.... comment your route and add it here 
